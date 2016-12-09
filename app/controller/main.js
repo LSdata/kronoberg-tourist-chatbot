c875140767eaea859a18ebce444756bb45b236ec
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-09 09:20:32 +0000

    edits..

--- app/controller/main.js
+++ app/controller/main.js
@@ -6,6 +6,7 @@
 var path = require('path');
 var appJS = require('../../app.js');
 var appJS = require(path.join(__dirname, '/../../app.js'))
+var chats = require(path.join(__dirname, '/../chats.js'))
 
 
 const 
