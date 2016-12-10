7ace9deb791b89d055dfa2212f850539bbcd66d6
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-10 14:17:02 +0000

    edits..

--- app/dataModel/chit-chat.js
+++ app/dataModel/chit-chat.js
@@ -4,14 +4,18 @@ var fbGraph = require(path.join(__dirname, 'fbGraph.js'))
 module.exports = {
     semEval: function(messageText){
 
-        if (messageText.indexOf('room') > -1) {
+        if ( (messageText.indexOf('room' || 'accomodation' || 'hotel') > -1)  ) {
           return "Are you looking for accomodations? ";
         } 
-        if ((messageText == "hi") || (messageText == "Hi")){
+        if ((messageText == "hi") || (messageText == "Hi") || (messageText == "Hi!")|| (messageText == "hi!") || 
+            (messageText == "hello")|| (messageText == "Hello")|| (messageText == "Hello!") || 
+            (messageText == "hello!") || (messageText == "hey") || (messageText == "Hey") || 
+            (messageText == "hey!") || (messageText == "Hey!") || (messageText == "Hi there!") || 
+            (messageText == "hi there") || (messageText == "hi :)") || (messageText == "hi there!")){
             return "Hi! :)";
         }
         else {
-          return "I didn't understand that. Here is a list of common topics many visitors are searching for: ";
+          return "I'm sorry I didn't quite get that. \nBut here is a list of common topics many visitors are searching for: ";
         }
     }
 };
