a1e1c0499450ee5c4cd99b7ee06ab3043a3d1e36
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-10 14:40:19 +0000

    edits..

--- app/dataModel/chit-chat.js
+++ app/dataModel/chit-chat.js
@@ -11,7 +11,7 @@ module.exports = {
             (messageText.indexOf('spend the night') > -1)) {
           return "Are you looking for accomodations? ";
         } 
-        if(capitalTxt.indexOf('food'.toUpperCase() > -1) ){
+        if(capitalTxt.indexOf(('food').toUpperCase() > -1) ){
             return ("do you want suggestions on were to eat?");
         }
         if ((messageText == "hi") || (messageText == "Hi") || (messageText == "Hi!")|| (messageText == "hi!") || 
