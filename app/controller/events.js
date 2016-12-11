9596abff0a3a750671a87a958bc71f7f9a96351e
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-11 11:33:33 +0000

    reply..

--- app/controller/events.js
+++ app/controller/events.js
@@ -161,8 +161,7 @@ module.exports = {
     
       // When a postback is called, we'll send a message back to the sender to 
       // let them know it was successful
-      //chat_info.sendTextMessage(senderID, "Postback called");
-      chat_info.sendTextMessage( senderID, chat_info.startGreetings() );
+      chat_info.sendTextMessage(senderID, "Hi {{user_first_name}}!! :) Welcome to this bot! :)");
     },
 
 /*
