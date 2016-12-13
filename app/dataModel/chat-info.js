099778b3b3453c22c6ff59f07e1d85368906aec8
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-13 12:23:47 +0000

    edits..

--- app/dataModel/chat-info.js
+++ app/dataModel/chat-info.js
@@ -232,7 +232,7 @@ module.exports = {
                 subtitle: parsed['results'][0].formatted_address,
                 buttons: [{
                   type: "web_url",
-                  url: parsed['results'][0].website,
+                  url: "https://www.oculus.com/en-us/rift/",
                   title: "Open Web URL"
                 }],
               }, {
