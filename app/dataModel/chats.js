6e46c6c0a86382ad3ea4fae1c98d91ba068e05fd
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-14 19:03:32 +0000

    edits..

--- app/dataModel/chats.js
+++ app/dataModel/chats.js
@@ -34,8 +34,7 @@ module.exports = {
             googleAPI.google_eatings(searchquery, type, function(response){
                 //console.log(response);
                 //chat_info.sendTextMessage(senderID, response);
-                //chat_info.generic(senderID, response);
-                chat_info.sendGenericMessage(senderID);
+                chat_info.generic(senderID, response);
             });        
             return "ok"
         }
