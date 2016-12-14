9ec565ea3fc71f04ba7bd2866ec815200c63f96d
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-14 18:45:43 +0000

    edits..

--- app/dataModel/chats.js
+++ app/dataModel/chats.js
@@ -31,11 +31,12 @@ module.exports = {
         if(capitalTxt == 'GEO'){
             var searchquery = 'kronoberg'; //not åäö --> aao as Vaxjo
             var type = 'bakery|restaurant|cafe';
-            googleAPI.google_eatings(searchquery, type, function(response){
+            googleAPI.google_eatings(searchquery, type, function(response, next){
                 //console.log(response);
                 //chat_info.sendTextMessage(senderID, response);
                 //chat_info.generic(senderID, response);
                 chat_info.sendGenericMessage(senderID);
+                next();
             });        
             return ""
         }
