44835bbc0ab40d2471f43b822e32906dc1446dec
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-16 14:31:04 +0000

    edits..

--- app/dataModel/chats.js
+++ app/dataModel/chats.js
@@ -24,6 +24,8 @@ module.exports = {
             (capitalTxt.indexOf('HUNGRY') > -1) ){
                 var type = 'restaurant';
                 googleAPI.getPlaces(type, function(response){
+                    if(response)
+                        console.log("RESPONSE EXISTS")
                     chat_info.generic(senderID, response);
                 }); 
             return "ah do you want suggestions of where to eat? 🍽  (Tap on the adress to see it on a map)";
