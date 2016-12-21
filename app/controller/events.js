72e91c4083f19b7ef65440e4edf76dc006e5bdde
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-21 08:30:55 +0000

    edits..

--- app/controller/events.js
+++ app/controller/events.js
@@ -158,13 +158,13 @@ module.exports = {
       
       if (payload =="USER_DEFINED_PAYLOAD"){
         chat_info.pers_startmenu();
-        chat_info.sendTextMessage(senderID, "Hi!! :) Welcome! How can I help you today? What are you looking for in Kronoberg?");
-      }
-      else if (payload =="start"){
+        //chat_info.sendTextMessage(senderID, "Hi!! :) Welcome! How can I help you today? What are you looking for in Kronoberg?");
         chat_info.userName(senderID, function(response){
           chat_info.sendTextMessage(senderID, "Hi and welcome "+response+"!! :) \nHow can I help you today? \nWhat are you looking for in Kronoberg?");
         }); 
-        //chat_info.sendTextMessage(senderID, "Hi and welcome!! :) \nHow can I help you today? \nWhat are you looking for in Kronoberg?");
+      }
+      else if (payload =="start"){
+        chat_info.sendTextMessage(senderID, "Hi and welcome!! :) \nHow can I help you today? \nWhat are you looking for in Kronoberg?");
       }
       else if (payload =="accomodation"){
         var botReply = chitchat.semEval(senderID, "accomodation");
