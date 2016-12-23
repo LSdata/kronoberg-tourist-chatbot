b38b373b140581d439fa948cb7d36a2d44cea245
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-23 19:55:41 +0000

    edits..

--- app/dataModel/chat-info.js
+++ app/dataModel/chat-info.js
@@ -321,7 +321,7 @@ module.exports = {
                     "default_action": {
                         "type": "web_url",
                         "url": "https://www.onskefoto.se",
-                        "messenger_extensions": true,
+                        //"messenger_extensions": true,
                         "webview_height_ratio": "tall",
                         //"fallback_url": "https://www.onskefoto.se"
                     },
@@ -330,7 +330,7 @@ module.exports = {
                             "title": "details",
                             "type": "web_url",
                             "url": "https://www.onskefoto.se",
-                            "messenger_extensions": true,
+                            //"messenger_extensions": true,
                             "webview_height_ratio": "tall",
                             //"fallback_url": "https://www.onskefoto.se"                        
                         }
