d4c380f204e402cdbd26c4845b325b9081876f91
Linnea Strågefors <mail.linnea.s@gmail.com>
2016-12-25 10:07:03 +0000

    edits..

--- app/dataModel/weatherAPI.js
+++ app/dataModel/weatherAPI.js
@@ -49,7 +49,9 @@ module.exports = {
               weatherArr[2] = [];
               weatherArr[2]['dayName'] = weather2d.forecast.txt_forecast.forecastday[4].title;
               weatherArr[2]['img'] = weather2d.forecast.txt_forecast.forecastday[4].icon_url;
-              weatherArr[2]['fcTxt'] = weather2d.forecast.txt_forecast.forecastday[2].fcttext_metric;
+              weatherArr[2]['fcTxt'] = weather2d.forecast.txt_forecast.forecastday[4].fcttext_metric;
+              //weatherArr[2]['fcTxt'] = weather2d.forecast.simpleforecast.forecastday[1];
+              console.log("WEATHER: "+weather2d.forecast.simpleforecast.forecastday[1]);
               
               callback(weatherArr);
               
